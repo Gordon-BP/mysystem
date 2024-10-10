348e12a3d2a7705615b1bc5584eb3eca6aa6b852
Gordon-BP <gordynumberone@protonmail.com>
2024-10-10 19:12:40 +0800

    rm picosnitch

--- usrModules/gordy.nix
+++ usrModules/gordy.nix
@@ -28,7 +28,6 @@ in
      ngrok # Expose local ports to the internet
      obs-studio # Screen recording and streaming
      ollama # Local LLM server app
-     picosnitch # application-level firewall, like LuLu
      ruff # python linter
      transmission_4 # OSS bittorrent
      vimPlugins.catppuccin-nvim # Catpuccin theme for neovim
